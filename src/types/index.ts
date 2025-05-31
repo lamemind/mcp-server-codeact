8f1049a664b4966d0cb225795194d9856361971b
lamemind <lamemind@gmail.com>
2025-05-31 08:23:08 +0200

    refactor: clean up exports in index.ts by removing commented sections and unused schema

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,5 @@
-// Operations types
 export * from './act-operations-schema.js';
 
-// Batch management types  
 export * from './tool-batch-schema.js';
 
-// Configuration schema types
-export * from './server-config-schema.js';
+export * from './internal-types.js';
