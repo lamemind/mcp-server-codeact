ddfe8816d3515afd70cc8cf29f1953ef3898e9cf
lamemind <lamemind@gmail.com>
2025-06-04 23:33:57 +0200

    fix: update server startup message and handle optional config file in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 
 import { parseCliArguments } from "./server/cli-arguments.js";
-import { readConfigFile } from "./server/json-config.js";
+import { DEFAULT_CONFIG, readConfigFile } from "./server/json-config.js";
 import { startMcpServer } from "./server/mcp-server.js";
 
 
 (async function () {
-    console.error(`Starting MCP Server Wrapper...`);
+    console.error(`Starting MCP Server CodeAct...`);
     const cliArguments = await parseCliArguments();
 
-    const serverConfig = readConfigFile(cliArguments.configFile);
+    const serverConfig = cliArguments.configFile
+        ? readConfigFile(cliArguments.configFile)
+        : DEFAULT_CONFIG
     console.error(serverConfig);
 
     await startMcpServer(serverConfig);
